171e2d2e4786334b3ac340517ffbf3ce2c07da7f
Paul Kelders <p_kelders@hotmail.com>
2021-10-08 15:10:28 +0200

    Added pattern matching

--- net-5-and-6-samples/Program.cs
+++ net-5-and-6-samples/Program.cs
@@ -2,8 +2,23 @@
 
 Console.WriteLine("Hello, .NET 5 and 6");
 
-Man man1 = new("Peter", "Pan");
-Man man2 = new("Captain", "Hook");
+Console.WriteLine("---- Records ----");
+
+Man man1 = new("Peter", "Pan", 12);
+Man man2 = new("Captain", "Hook", 42);
 
 Console.WriteLine(man1);
 Console.WriteLine(man2);
+
+Man man3 = man1;
+
+Console.WriteLine(man3);
+
+Console.WriteLine(man3 == man1);
+
+Console.WriteLine("---- Pattern matching ----");
+
+Console.WriteLine(man3 is Man);
+Console.WriteLine(man3 is Woman);
+Console.WriteLine(man3.Age is > 10 and <40);
+Console.WriteLine(man3 is Person);
--- net-5-and-6-samples/Records.cs
+++ net-5-and-6-samples/Records.cs
@@ -1,8 +1,8 @@
 ﻿namespace net_5_and_6_samples
 {
     public enum PersonSex { Unknown = 0, Man = 1, Woman = 2 };
-    public record Person(string FirstName, string LastName, PersonSex Sex);
+    public record Person(string FirstName, string LastName, PersonSex Sex, int Age);
 
-    public record Man(string FirstName, string LastName) : Person(FirstName, LastName, PersonSex.Man);
-    public record Woman(string FirstName, string LastName) : Person(FirstName, LastName, PersonSex.Woman);
+    public record Man(string FirstName, string LastName, int Age) : Person(FirstName, LastName, PersonSex.Man, Age);
+    public record Woman(string FirstName, string LastName, int Age) : Person(FirstName, LastName, PersonSex.Woman, Age);
 }
