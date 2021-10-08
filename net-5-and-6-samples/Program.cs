bcf88947d67acbb1a6a6efbb20faedc86f9d97bd
Paul Kelders <p_kelders@hotmail.com>
2021-10-08 15:04:01 +0200

    Add project files.

---
+++ net-5-and-6-samples/Program.cs
@@ -0,0 +1,9 @@
+﻿using net_5_and_6_samples;
+
+Console.WriteLine("Hello, .NET 5 and 6");
+
+Man man1 = new("Peter", "Pan");
+Man man2 = new("Captain", "Hook");
+
+Console.WriteLine(man1);
+Console.WriteLine(man2);
---
+++ net-5-and-6-samples/Records.cs
@@ -0,0 +1,8 @@
+﻿namespace net_5_and_6_samples
+{
+    public enum PersonSex { Unknown = 0, Man = 1, Woman = 2 };
+    public record Person(string FirstName, string LastName, PersonSex Sex);
+
+    public record Man(string FirstName, string LastName) : Person(FirstName, LastName, PersonSex.Man);
+    public record Woman(string FirstName, string LastName) : Person(FirstName, LastName, PersonSex.Woman);
+}
